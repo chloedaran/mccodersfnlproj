bf47152b928d22c7640bcbd29a4a0eb1262e60b8
chloedaran <91086957+chloedaran@users.noreply.github.com>
2021-12-02 20:21:58 -0500

    set up
    set up

--- #Final Project.py
+++ #Final Project.py
@@ -1,1 +1,20 @@
 #Final Project 
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import requests
+import re
+import os
+import csv
+import sqlite3
+import json
+
+cid = '5e6f0741afdd4e2892e4aa3dacd22f8a'
+secret = '803eb81ea146492fb5ec1edca8e1b9d5'
+
+ccm = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager = ccm)
+
+
+
+
